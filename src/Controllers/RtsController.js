f8431ceb56becbcfb16a563479b754e293b78f9d
manupandelo <manuelpandelo12@gmail.com>
2023-02-25 04:21:46 -0300

    aaaaa

--- src/Controllers/RtsController.js
+++ src/Controllers/RtsController.js
@@ -9,6 +9,8 @@ router.get('', async (req, res) => {
   return res.status(200).json("Bienvenido al sistema de RTS Commisioning");
 });
 
+// GETS
+
 router.get('/sistema', async (req, res) => {
   console.log("Get Sistemas")
   const rows = await rtsService.getSistemas();
@@ -36,7 +38,6 @@ router.get('/tarea', async (req, res) => {
 router.get('/registro-tareas', async (req, res) => {
   console.log("Get Registro Tareas")
   const rows = await rtsService.getRegistros();
-  console.log(rows.length)
   return res.status(200).json(rows);
 });
 
@@ -45,5 +46,46 @@ router.get('/registro-tareas/:id', async (req, res) => {
   const rows = await rtsService.getRegistroById(req.params.id);
   return res.status(200).json(rows);
 });
+
+// POSTS
+
+router.post('/proyecto', async (req, res) => {
+  console.log("Post Proyecto")
+  const rows = await rtsService.postProyecto(req.body.nombre);
+  return res.status(200).json(rows);
+});
+
+router.post('/sistema', async (req, res) => {
+  console.log("Post Sistema")
+  const rows = await rtsService.postSistema(req.body);
+  return res.status(200).json(rows);
+});
+
+router.post('/subsistema', async (req, res) => {
+  console.log("Post Subsistema")
+  const rows = await rtsService.postSubSistema(req.body);
+  return res.status(200).json(rows);
+});
+
+router.post('/tag', async (req, res) => {
+  console.log("Post Tag")
+  const rows = await rtsService.postTag(req.body);
+  return res.status(200).json(rows);
+});
+
+router.post('/tarea', async (req, res) => {
+  console.log("Post Tarea")
+  const rows = await rtsService.postTarea(req.body);
+  return res.status(200).json(rows);
+});
+
+// PUTS
+
+router.put('/registro-tareas/:id', async (req, res) => {
+  console.log("Put Registro Tareas")
+  const rows = await rtsService.putRegistro(req.params.id, req.body);
+  return res.status(200).json(rows);
+});
+
 
 export default router;
--- src/Services/RtsService.js
+++ src/Services/RtsService.js
@@ -4,6 +4,9 @@ import con from '../../db.js'
 const connection = con
 
 export class RtsService {
+
+    // GETS
+
     getSistemas = async () => {
         let query = `SELECT sistema.id, sistema.nombre, sistema.idsistema, proyecto.nombre as proyecto FROM sistema INNER JOIN proyecto ON sistema.idproyecto = proyecto.idproyecto`
         const [result, fields] = await connection.execute(query)
@@ -18,8 +21,23 @@ export class RtsService {
 
     getTags = async () => {
         let query = `SELECT tag.idtag as id, tag.tag, tag.nombre, tag.idtipo, subsistema.nombre as subsistema, subsistema.numsubsistema, tag.plano, especialidad.nombre as especialidad, tipo.nombre as tipo, tag.observaciones FROM tag INNER JOIN subsistema ON tag.idsubsistema = subsistema.id INNER JOIN especialidad ON tag.idespecialidad = especialidad.idespecialidad INNER JOIN tipo ON tag.idtipo = tipo.idtipo`
-        const [result, fields] = await connection.execute(query)
-        return result
+        let query2 = `SELECT tagXtarea.id, tagXtarea.idtag, tag.tag, tag.nombre as nombretag, tarea.nombre as tarea, tagXtarea.realizado from tagXtarea INNER JOIN tag ON tagXtarea.idtag = tag.idtag INNER JOIN tarea ON tagXtarea.idtarea = tarea.idtarea order by tagXtarea.id asc`
+        const [Tags, fields] = await connection.execute(query)
+        const [registro, fields2] = await connection.execute(query2) 
+        for(let i = 0; i < Tags.length; i++) {
+            Tags[i].tareas = []
+            let suma = 0
+            for(let j = 0; j < registro.length; j++) {
+                if(Tags[i].id == registro[j].idtag) {
+                    Tags[i].tareas.push(registro[j])
+                }
+            }
+            for(let k = 0; k < Tags[i].tareas.length; k++) {
+                suma += Tags[i].tareas[k].realizado
+            }
+            Tags[i].filledQuantity = suma / Tags[i].tareas.length * 100
+        }
+        return Tags
     }
 
     getTareas = async () => {
@@ -31,14 +49,49 @@ export class RtsService {
     getRegistros = async () => {
         let query = `select tagXtarea.id, tag.tag, tag.nombre as nombretag, tarea.nombre as tarea, tagXtarea.realizado from tagXtarea INNER JOIN tag ON tagXtarea.idtag = tag.idtag INNER JOIN tarea ON tagXtarea.idtarea = tarea.idtarea order by tagXtarea.id asc`
         const [result, fields] = await connection.execute(query)
-
         return result
     }
 
-    getRegistroById = async (id) => {
-        let query = `SELECT tagXtarea.id, tag.nombre as nombretag, tag.tag, tarea.nombre as tarea, tagXtarea.realizado FROM tagXtarea inner join tag on tagXtarea.idtag = tag.idtag inner join tarea on tagXtarea.idtarea = tarea.idtarea WHERE idTag = ? order by tagXtarea.id asc`  
-        const [result, fields] = await connection.execute(query, [id])
+
+    // POSTS
+
+    postProyecto = async (nombre) => {
+        let query = `INSERT INTO proyecto (nombre) VALUES (?)`
+        const [result, fields] = await connection.execute(query, [nombre])
         return result
     }
+
+    postSistema = async (sistema) => {
+        let query = `INSERT INTO sistema (nombre, idsistema, idproyecto) VALUES (?, ?, ?)`
+        const [result, fields] = await connection.execute(query, [sistema.nombre, sistema.idsistema, sistema.idproyecto])
+        return result
+    }
+
+    postSubSistema = async (subSistema) => {
+        let query = `INSERT INTO subsistema (numsubsistema, fechainicio, fechafinal, nombre, idsistema) VALUES (?, ?, ?, ?, ?)`
+        const [result, fields] = await connection.execute(query, [subSistema.numsubsistema, subSistema.fechainicio, subSistema.fechafinal, subSistema.nombre, subSistema.idsistema])
+        return result
+    }
+
+    postTag = async (tag) => {
+        let query = `INSERT INTO tag (tag, nombre, idtipo, idsubsistema, plano, idespecialidad, observaciones) VALUES (?, ?, ?, ?, ?, ?, ?)`
+        const [result, fields] = await connection.execute(query, [tag.tag, tag.nombre, tag.idtipo, tag.idsubsistema, tag.plano, tag.idespecialidad, tag.observaciones])
+        return result
+    }
+
+    postTarea = async (tarea) => {
+        let query = `INSERT INTO tarea (nombre, idtipo, codigo, idespecialidad, ubicacion) VALUES (?, ?, ?, ?, ?)`
+        const [result, fields] = await connection.execute(query, [tarea.nombre, tarea.idtipo, tarea.codigo, tarea.idespecialidad, tarea.ubicacion])
+        return result
+    }
+
+    // PUTS
+
+    putRegistro = async (id, registro) => {
+        let query = `UPDATE tagXtarea SET realizado = ? WHERE id = ?`
+        const [result, fields] = await connection.execute(query, [registro.realizado, id])
+        return result
+    }
+
 
 }
